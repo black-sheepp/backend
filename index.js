d8e0f41ad8b1b13bb827068b46f42ec9c2618ded
Shivam <shivamguptanitw@gmail.com>
2023-09-14 13:11:38 +0530

    Server running fine and tested

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const dotenv = require('dotenv').config();
-const path = require('path');
-const express = require('express');
-const db = require('./config/mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const dotenv = require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const db = require("./config/mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 8000 ;
+const PORT = process.env.PORT || 8000;
 
 // Middleware configuration
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Route configuration
-app.get('/', (req, res) =>{
-    res.send("Welcome Home!");
-})
+app.get("/", (req, res) => {
+	res.send("Welcome Home!");
+});
 
-app.listen(PORT,()=>{
-    console.log('Server is up and running on port '+PORT);
-})
+app.listen(PORT, () => {
+	console.log("Server is up and running on port " + PORT);
+});
